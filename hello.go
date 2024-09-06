e34267bf7314395fe4f99817f27084e708738fa3
radmila gubaidullina <radmilagubajdullina844@gmail.com>
2024-09-06 14:45:00 +0300

    прост разбираюсь

--- hello.go
+++ hello.go
@@ -2,21 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
 )
 
 func main() {
-	var n int
-	_, err := fmt.Scan(&n)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	calculate(3, 4)
 
-	for i := 0; i < n; i++ {
-		x := 1 << i
-		if x > n {
-			break
-		}
-		fmt.Print(x, " ")
-	}
+}
+func calculate(a, b int) {
+	fmt.Println(a + b)
 }
