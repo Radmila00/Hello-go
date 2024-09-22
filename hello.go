50b2291185cad99d08c0619d9d2d4ba9619fc709
radmila gubaidullina <radmilagubajdullina844@gmail.com>
2024-09-22 08:28:41 +0300

    игра "Угадай число"

--- hello.go
+++ hello.go
@@ -1,18 +1,1 @@
 package main
-
-import (
-	"fmt"
-)
-
-func main() {
-	fmt.Println(sumInt(1, 0))
-
-	print(5, 6)
-}
-func sumInt(a ...int) (n int, sum int) {
-	for _, v := range a {
-		n = n + 1
-		sum = sum + v
-	}
-	return n, sum
-}
--- main.go
+++ main.go
@@ -1,8 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	//"time"
+)
 
-func print(c, b int) int {
-	fmt.Println(c + b)
-	return 0
+func main() {
+	var number = rand.Intn(10)
+	fmt.Println(number)
+	fmt.Println("Привет,это игра 'Угадай число'.У тебя 3 попытки:) ")
+	fmt.Println("Введите ваше число")
+	count := 0
+	player_number1 := -1
+
+	for count < 3 && number != player_number1 {
+
+		fmt.Scan(&player_number1)
+		if count >= 1 && number != player_number1 {
+			if number < player_number1 {
+				fmt.Println("Загаданное число меньше")
+			} else if number > player_number1 {
+				fmt.Println("Загаданное число больше")
+			}
+		} else if count == 0 {
+			fmt.Println("Подумай еще")
+		}
+		count++
+
+	}
+	if number == player_number1 {
+		fmt.Println("Верно!")
+	} else {
+		fmt.Println("Упс...ты проиграл")
+	}
+
 }
