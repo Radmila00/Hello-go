35ef8a795af22e4c3abd8765b35fc3c98adea72f
radmila gubaidullina <radmilagubajdullina844@gmail.com>
2024-09-28 15:42:17 +0300

    до непоправимого

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	var number = rand.Intn(10)
-	fmt.Println(number)
+	/*fmt.Println(number)*/
 	fmt.Println("Привет,это игра 'Угадай число'.У тебя 3 попытки:) ")
 	fmt.Println("Введите ваше число")
 	count := 0
@@ -17,22 +17,26 @@ func main() {
 	for count < 3 && number != player_number1 {
 
 		fmt.Scan(&player_number1)
-		if count >= 1 && number != player_number1 {
+		if count < 2 && number != player_number1 {
 			if number < player_number1 {
 				fmt.Println("Загаданное число меньше")
 			} else if number > player_number1 {
 				fmt.Println("Загаданное число больше")
 			}
-		} else if count == 0 {
-			fmt.Println("Подумай еще")
+		} else if count == 2 && number != player_number1 {
+			fmt.Println("Попытки закончились")
 		}
 		count++
 
 	}
+
 	if number == player_number1 {
 		fmt.Println("Верно!")
 	} else {
 		fmt.Println("Упс...ты проиграл")
+	}
+	if count == 3 && number != player_number1 {
+		fmt.Println("Загаданное число=", number)
 	}
 
 }
